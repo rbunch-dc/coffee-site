23fcb30510a53a36996e449c39225b1896704341
Robert Bunch <rdbunch@gmail.com>
2015-12-18 17:38:58 -0500

    switch to mongo labs uri

--- app.js
+++ app.js
@@ -44,7 +44,12 @@ passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
 //Mongoose
-mongoose.connect('mongodb://localhost:27017/ecommerce')
+ var mongoUrl =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost:27017/coffee';
+
+
 
 app.use('/', routes);
 app.use('/users', users);
